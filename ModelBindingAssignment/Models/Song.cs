4a5e26a2165b786b09f3577fc727ce5e7916eb22
Cameron White <cameronwhite4121@gmail.com>
2024-07-17 18:35:43 -0700

    Added validation for song's fields

--- ModelBindingAssignment/Models/Song.cs
+++ ModelBindingAssignment/Models/Song.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection.Metadata.Ecma335;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ModelBindingAssignment.Models
 {
@@ -10,24 +11,31 @@ namespace ModelBindingAssignment.Models
 		/// <summary>
 		/// Song's title, or name
 		/// </summary>
+		[Required(ErrorMessage = "Song name can't be empty")]
 		public string Name { get; set; }
 
 		/// <summary>
 		/// Album song was featured in. If the song was a single,
 		/// "Single" should be the value for Album.
 		/// </summary>
+		[Required(ErrorMessage = "Song album can't be empty")]
 		public string Album { get; set; }
 
 		/// <summary>
 		/// Name of the band or the artist that the song
 		/// is credited to.
 		/// </summary>
+		[Required(ErrorMessage = "Song artist can't be empty")]
 		public string Artist { get; set; }
 
 		/// <summary>
 		/// Length of the song in minutes. Rounded off to 2
-		/// decimal places.
+		/// decimal places. Length can't be negative but can
+		/// be 0 for cases when song length isn't known yet.
+		/// Max value is 8400 or 140 hrs. Worlds longest song
+		/// is 138 hours long. That's a real fact.
 		/// </summary>
+		[Range(0, 8400)]
 		public double Length 
 		{
 			get { return Length; }
