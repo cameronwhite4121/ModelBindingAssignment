9beba3fc79a7d9bac39607a195b49df3e400103b
Cameron White <cameronwhite4121@gmail.com>
2024-07-17 18:22:47 -0700

    Added necessary parts of song form

--- ModelBindingAssignment/Controllers/SongController.cs
+++ ModelBindingAssignment/Controllers/SongController.cs
@@ -1,12 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ModelBindingAssignment.Models;
 
 namespace ModelBindingAssignment.Controllers
 {
     public class SongController : Controller
     {
+        [HttpGet]
         public IActionResult CreateSong()
         {
             return View();
         }
-    }
+	}
 }
