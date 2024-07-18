670465826f65919f6cdc586bcaf011e5f0686270
Cameron White <cameronwhite4121@gmail.com>
2024-07-17 17:34:28 -0700

    Added song class with properties

---
+++ ModelBindingAssignment/Models/Song.cs
@@ -0,0 +1,38 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace ModelBindingAssignment.Models
+{
+	/// <summary>
+	/// Represents one song
+	/// </summary>
+	public class Song
+	{
+		/// <summary>
+		/// Song's title, or name
+		/// </summary>
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Album song was featured in. If the song was a single,
+		/// "Single" should be the value for Album.
+		/// </summary>
+		public string Album { get; set; }
+
+		/// <summary>
+		/// Name of the band or the artist that the song
+		/// is credited to.
+		/// </summary>
+		public string Artist { get; set; }
+
+		/// <summary>
+		/// Length of the song in minutes. Rounded off to 2
+		/// decimal places.
+		/// </summary>
+		public double Length 
+		{
+			get { return Length; }
+			set { Length = Math.Round(value, 2); }
+		}
+
+	}
+}
